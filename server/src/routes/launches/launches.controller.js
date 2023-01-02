f7ea103074ef18e4178c9f6993e666cea9e03a9c
Navdip Hidad <navdeephidad@gmail.com>
2023-01-02 17:02:39 +0530

    Complete Nasa project with incomplete API testing

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,35 +1,34 @@
-const {getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById} = require('../../models/launches.model')
+const { getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById } = require('../../models/launches.model')
 
-function httpGetAllLaunches(req, res){
+function httpGetAllLaunches(req, res) {
     res.status(200).json(getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res){
+function httpAddNewLaunch(req, res) {
     const launch = req.body;
-
-    // if(isNaN(launch.launchDate)){
-    //     return res.status(400).json({
-    //         error: 'Invalid launch date!',
-    //     });
-    // }
-
-    // if(!launch.mission || !launch.rocket || !launch.launchDate
-    //     || !launch.target){
-    //     return res.status(400).json({
-    //         error: 'Missing required launch property',
-    //     });
-    // }
-
     launch.launchDate = new Date(launch.launchDate);
+
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        });
+    }
+
+    if (!launch.mission || !launch.rocket || !launch.launchDate
+        || !launch.target) {
+        return res.status(400).json({
+            error: 'Missing required launch property',
+        });
+    }
 
     addNewLaunch(launch);
     return res.status(201).json(launch);
 }
 
-function httpAbortLaunch(req, res){
+function httpAbortLaunch(req, res) {
     const launchId = Number(req.params.id);
 
-    if(!existsLaunchWithId(launchId)){
+    if (!existsLaunchWithId(launchId)) {
         return res.status(404).json({
             error: 'Launch not found!',
         });
